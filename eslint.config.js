2c4a2589d81ff21265379b960e766bf2deb9f18c
GameOver960 <hatemmohmmed73@gmail.com>
2025-07-28 16:28:16 +0400

    test

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
+import js from '@eslint/js';
+
 export default [
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     rules: {
+      ...js.configs.recommended.rules,
       // Add custom rules here if needed
     },
   },
